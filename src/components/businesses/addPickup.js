e47663f6d00796b2ed6ee8486a00479e7bce079c
avpimblesr <allan.pimble@gmail.com>
2020-05-27 16:15:06 -0400

    I made a few cosmetic changes to my code.

--- src/components/businesses/addPickup.js
+++ src/components/businesses/addPickup.js
@@ -1,20 +1,13 @@
-//this is a form that will allow the business to add a pickup to their profile
-//when the business creates a new pickup it will also display on the pickup list
-//it will also display on the business profile
+//this is a form that will allow the business to add a pickup to their profile when the business creates 
+// a new pickup it will also display on the pickup list it will also display on the business profile
 
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router-dom'
 import { Button, ButtonGroup, TextField } from '@material-ui/core'
-
-import { axiosWithAuth } from '../../utils/axiosWithAuth';
-
+import { axiosWithAuth } from '../../utils/axiosWithAuth'
 
 const AddPickup = () => {
-  // Extract the named exports from the useForm hook that are needed for this form
-  // The useForm hook handles the form management including state
-
-
   // Set the default values for the form
   const { register, errors, handleSubmit } = useForm({
     mode: "onChange",
@@ -26,42 +19,45 @@ const AddPickup = () => {
     }
   })
 
+  // register attaches our fields to the state by use of the 'ref' attribute of the field
+  // Because I we are using Material UI, inputRef replaces the ref attribute when using
+  // the useForm hook to manage the form
 
   // Create the callback for the handleSubmit function. The parameter (whatever I name it)
   // will contain all the form's data.
   const onSubmit = (formData) => {
+
     //   e.preventDefault()
     axiosWithAuth()
       .post("pickups", formData)
       .then((res) => {
-        console.log(res);
-
-
+        console.log(res)
       })
-      .catch((err) => console.log(err.response));
+      .catch((err) => console.log(err.response))
   }
-
 
   // Keep track of routing history
   let history = useHistory()
 
   // Go back to calling page
   const goBack = () => history.goBack()
+  const typeErrMessage = 'Please describe what is to be picked up'
+  const amountErrMessage = 'Please provide a quantity'
 
   return (
     <div>
       <h2>Add a Pickup</h2>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-
         <label htmlFor='amount'>Amount: </label>&nbsp;
         <TextField
           type='text'
           name='amount'
           placeholder='amount'
-          inputRef={register({ required: 'Please provide a quantity', minLength: 5 })} />
+          inputRef={register({ required: { amountErrMessage }, minLength: 5 })} />
         <br />
-        {errors.amount && <p>{errors.amount.message}</p>}<br />
+        {errors.amount && <p>{errors.amount.message}</p>}
+        <br />
 
         <label htmlFor='pickup-date'>Pickup Date: </label>&nbsp;
         <TextField
@@ -76,7 +72,8 @@ const AddPickup = () => {
           type='text'
           name='type'
           placeholder='type of pickup'
-          inputRef={register({ required: 'Please describe what is to be picked up', minLength: 5 })} /><br />
+          inputRef={register({ required: { typeErrMessage }, minLength: 5 })} />
+        <br />
         {errors.type && <p>{errors.type.message}</p>}
         <br /><br />
 
@@ -88,59 +85,14 @@ const AddPickup = () => {
           <Button>Add Pickup</Button>
           <Button type='button' onClick={goBack}>Go Back</Button>
         </ButtonGroup>
-
-
       </form>
+      {/*      
       <button onClick={() => push("/business-profile")}>
         Return To Profile
       </button>
+*/}
     </div>
   );
 };
 
 export default AddPickup;
-
-
-
-
-// ~~ N O T E S ~~ //
-// [ Web Unit 2 ]
-// [ Skills Based Assessment - 2+ ON EACH OBJECTIVE REQUIRED TO PASS ]
-
-// -- Rubric Item 1: Applied JavaScript
-// 2 ==> Student created functional components and used events in application to add dynamic functionality to app.
-// 3 ==> Student incorperated a third party event/animation library like unto Greensock, Anime, React-motion etc.
-
-// -- Rubric Item 2: Intro to React
-// 2 ==> Student's code was organized at the component level, proper usage of state and props are demonstrated
-//       throughout the project, the UI is composed of small reusable components, proper usage of useState and
-//       useEffect hooks are clearly incorporated and correctly implemented.  Student used Array methods to
-//       dynamically render HTML elements.
-// 3 ==> Student was able to architect components to be easily reused. Student used advanced React techniques
-//       like the composition pattern, custom hooks, render props, HOCs, etc.
-
-// -- Rubric Item 3: Single Page Application
-// 2 ==> Student implemented GET requests using either Axios or Fetch to display 3rd party data on a deployed page.
-//       Route management properly installed and used to show top level pages as well as nested views where necessary.
-// 3 ==> Not only are standard network request techniques employed, the code is organized in such a fashion that
-//       the student demonstrated proper use of container vs presentational components or other industry standards,
-//       conventions or patterns.
-
-// -- Rubric Item 4: Form Management
-// 2 ==> Student has set up component management for the forms in the app that makes sense for each form.
-//       Student made the decision to use a third-party library, like Formik, or not, and can defend
-//       their decision. Some form validation is in place.
-// 3 ==> Student showed great insight in setting up the state management for the app's forms. Form validation
-//       is in place for all fields, and covers all use cases. Loading states and success/error notifications
-//       are in place and add to the overall UX of the app.
-
-//    T E A M W O R K   A S S E S S M E N T    //
-
-// -- MVP work - Project should incorporate all of the listed MVP features
-// 2 ==> Student's work demonstrates that all MVP features were built
-// 3 ==> Student's work demonstrates that all MVP features were built and the student went above and beyond
-//       the project.
-
-// -- Team Work Score
-// 2 ==> Team member was collaborative, able to work in a team environment
-// 3 ==> Pair programmed with the Web UI and Back end Architect
