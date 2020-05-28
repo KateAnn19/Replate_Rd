c32d560a475977ec1f53bae6b23442106a9e9a9f
avpimblesr <allan.pimble@gmail.com>
2020-05-28 16:18:17 -0400

    Rearranged the buttons

--- src/components/volunteers/pickup-list.js
+++ src/components/volunteers/pickup-list.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom"; //this can be used to easily navigate to a certain page
 
 // this is so the request is authenticated. Look at the addPickup form you created to see how to correctly implement this on your get request 
-import { axiosWithAuth } from "../../utils/axiosWithAuth"; 
+import { axiosWithAuth } from "../../utils/axiosWithAuth";
 
 import "../styles/pickup-list.css";
 
@@ -19,16 +19,16 @@ const PickUpList = () => {
   useEffect(() => {
     getPickupList();
   }, []);
-  
+
   const getPickupList = () => {
     axiosWithAuth()
-    .get("/pickups/unassigned")
-    .then((res) => {
-      setPickupList(res.data)
-    })
-    .catch((err) => console.error(err));
+      .get("/pickups/unassigned")
+      .then((res) => {
+        setPickupList(res.data)
+      })
+      .catch((err) => console.error(err));
   };
-  
+
   const { push } = useHistory();
 
 
@@ -40,42 +40,47 @@ const PickUpList = () => {
   //"business-name"
   //"business-address"
   //"pickup-date" 
-  
+
   //getting the data to display may be a little tricky. Remember there are two ways to access an object... with dot notation
   //and with bracket notation. 
-  
-  
+
+
   return (
-    <div> 
-    {pickupList.map(item => {
-      return (
-        <div key={item['pickup-id']}>
-        <h1>{item['business-name']}</h1>
-        <p>{item['business-address']}<br />
-        {item['business-phone']}<br />
-        {item['amount']} of {item['type']}<br />
-        {item['pickup-date'].slice(0,10)}</p>
-        </div>)
-    })}    
-    <button onClick={() => push("/volunteer-profile")}>
-    Back to Profile
-    </button>&nbsp;
-    <button onClick={() => console.log('Pickup accepted')}>Accept pickup</button>
+    <div>
+      {pickupList.map(item => {
+        return (
+          <div key={item['pickup-id']}>
+            <h1>{item['business-name']}</h1>
+            <p>{item['business-address']}<br />
+              {item['business-phone']}<br />
+              {item['amount']} of {item['type']}<br />
+              {item['pickup-date'].slice(0, 10)}</p>
+          </div>)
+      })}
+      <button
+        onClick={() => console.log('Pickup accepted')}>
+        Accept pickup
+      </button>
+
+      <button
+        onClick={() => push("/volunteer-profile")}>
+        Back to Profile
+    </button>
     </div>
-    );
-  };
-  
-  
-  export default PickUpList;
+  );
+};
+
+
+export default PickUpList;
 
 
   //----------------------------------------------------
   //this is just fake data for testing - can ignore
   //----------------------------------------------------
-  
-  
+
+
   // const onSubmit = (formData) => {
-  
+
   //   //   e.preventDefault()
   //   axiosWithAuth()
   //     .post("pickups", formData)
@@ -83,14 +88,14 @@ const PickUpList = () => {
   //       console.log(res)
   //     })
   //     .catch((err) => console.log(err.response))
-  
+
   //     // Clear the form
   //     console.log(register)
   //     reset()
   // }
-  
-  
-  
+
+
+
   // let fakePickups = [
   //   {
   //     type: "Bread",
